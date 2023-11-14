42d7bdff7751f38ef7264a602d0c6865fdc8ee98
Teja Koripella <tejakoripella@gmail.com>
2023-11-14 10:38:36 -0800

    Add voltage compensation and set statusFramePeriods

--- src/main/java/frc/robot/subsystems/ShooterBase.java
+++ src/main/java/frc/robot/subsystems/ShooterBase.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.StatusFrame;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
@@ -20,10 +21,14 @@ import frc.robot.constants.ShooterConstants.ShooterState;
 
 abstract class ShooterBase extends SubsystemBase {
   /** Creates a new ShooterBase. */
-  protected final TalonFX mFlyWheelMotor = new TalonFX(0);
-  protected final TalonFX mRollerMotor = new TalonFX(0);
+  protected final TalonFX mFlyWheelMotor = new TalonFX(ShooterConstants.FLYWHEEL_TALON_ID);
+  protected final TalonFX mRollerMotor = new TalonFX(ShooterConstants.ROLLER_TALON_ID);
+
   protected double mRollerSetpoint = ShooterConstants.ShooterState.HOME.mRollerRPM;
   protected double mFlywheelSetpoint = ShooterConstants.ShooterState.HOME.mFlyWheelRPM;
+
+  protected final TalonFXConfiguration mRollerConfig = new TalonFXConfiguration();
+  protected final TalonFXConfiguration mFlywheelConfig = new TalonFXConfiguration();
 
   protected ShooterBase() {
     mFlyWheelMotor.configFactoryDefault();
@@ -39,6 +44,15 @@ abstract class ShooterBase extends SubsystemBase {
     
     mFlyWheelMotor.setSensorPhase(ShooterConstants.FLYWHEEL_SENSOR_PHASE);
     mRollerMotor.setSensorPhase(ShooterConstants.ROLLER_SENSOR_PHASE);
+
+    mFlyWheelMotor.configVoltageCompSaturation(12.0);
+    mRollerMotor.configVoltageCompSaturation(12.0);
+
+    mFlyWheelMotor.setStatusFramePeriod(StatusFrame.Status_1_General, 255);
+    mRollerMotor.setStatusFramePeriod(StatusFrame.Status_1_General, 255);
+
+    mFlyWheelMotor.enableVoltageCompensation(true);
+    mRollerMotor.enableVoltageCompensation(true);
     
     mFlyWheelMotor.configIntegratedSensorInitializationStrategy(SensorInitializationStrategy.BootToZero);
     mRollerMotor.configIntegratedSensorInitializationStrategy(SensorInitializationStrategy.BootToZero);
@@ -63,34 +77,40 @@ abstract class ShooterBase extends SubsystemBase {
   }
 
   public double getRollerRPM(){
-    return mFlyWheelMotor.getSelectedSensorVelocity()*ShooterConstants.SENSOR_VEL_TO_ROLLER_RPM;
+    return mFlyWheelMotor.getSelectedSensorVelocity()*ShooterConstants.ENCODER_VEL_TO_ROLLER_RPM;
   }
 
   public double getFlywheelRPM(){
-    return mRollerMotor.getSelectedSensorVelocity()*ShooterConstants.SENSOR_VEL_TO_FLYWHEEL_RPM;
+    return mRollerMotor.getSelectedSensorVelocity()*ShooterConstants.ENCODER_VEL_TO_FLYWHEEL_RPM;
   }
   public void writeMotorDebugData(){
     SmartDashboard.putNumber("RollerRPMNativeUnits", mFlyWheelMotor.getSelectedSensorVelocity());
-    SmartDashboard.putNumber("FlyWheelRPMNativeUnits", mFlyWheelMotor.getSelectedSensorVelocity());
+    SmartDashboard.putNumber("FlyWheelRPMNativeUnits", mRollerMotor.getSelectedSensorVelocity());
 
-    SmartDashboard.putNumber("FlyWheelRPMReal", mFlyWheelMotor.getSelectedSensorVelocity()*ShooterConstants.SENSOR_VEL_TO_ROLLER_RPM);
-    SmartDashboard.putNumber("FlyWheelRPMNativeUnits", mFlyWheelMotor.getSelectedSensorVelocity()*ShooterConstants.SENSOR_VEL_TO_FLYWHEEL_RPM);
+    SmartDashboard.putNumber("FlyWheelRPMReal", mFlyWheelMotor.getSelectedSensorVelocity()*ShooterConstants.ENCODER_VEL_TO_ROLLER_RPM);
+    SmartDashboard.putNumber("RollerWheelRPMReal", mRollerMotor.getSelectedSensorVelocity()*ShooterConstants.ENCODER_VEL_TO_FLYWHEEL_RPM);
     
     SmartDashboard.putNumber("Flywheel Setpoint RPM", mFlywheelSetpoint);
-    SmartDashboard.putNumber("Flywheel Setpoint RPM", mFlywheelSetpoint);
+    SmartDashboard.putNumber("Rol Setpoint RPM", mFlywheelSetpoint);
+    SmartDashboard.updateValues();
   }
   abstract void writeControllerDebugData();
+  
   public void stopMotors(){
-    mFlyWheelMotor.set(TalonFXControlMode.Velocity, 0);
-    mRollerMotor.set(TalonFXControlMode.Velocity,0);
+    mFlyWheelMotor.set(TalonFXControlMode.Disabled, 0);
+    mRollerMotor.set(TalonFXControlMode.Disabled,0);
   }
 
   private double getExitAngle(){
-    Translation3d midPointLoc = Util.getNormalTranslation(ShooterConstants.ROLLER_LOC_M.minus(ShooterConstants.FLYWHEEL_LOC_M))
-    .times((ShooterConstants.FLYWHEEL_RADIUS_CM+ShooterConstants.FLYWHEEL_LOC_M.getX()*100+ShooterConstants.ROLLER_RADIUS_CM+ShooterConstants.ROLLER_LOC_M.getX()*100)/2)
+    /*
+     double midPointMag = ShooterConstants.ROLLER_LOC_M.minus(ShooterConstants.FLYWHEEL_LOC_M).getNorm()-ShooterConstants.FLYWHEEL_RADIUS_CM/100-ShooterConstants.ROLLER_RADIUS_CM/100
+    Translation3d midPointLoc = ShooterConstants.ROLLER_LOC_M.minus(ShooterConstants.FLYWHEEL_LOC_M)
+    .minus(ShooterConstants.FLYWHEEL_RADIUS_CM+ShooterConstants.ROLLER_RADIUS_CM)
     .plus(ShooterConstants.FLYWHEEL_LOC_M);
-
-    return Math.asin(midPointLoc.getZ()/midPointLoc.getNorm());
+    Math.asin(midPointLoc.getZ()/midPointLoc.getNorm())
+    return ;
+     */
+    return 2;
   }
 
   private double getExitVelMPS(){
